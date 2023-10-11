eb19f31ba7f31c75af97bc94387b851fe3edb269
WalkingOnGreenball <walkingongreenball@gmail.com>
2023-10-11 12:20:01 +0900

    [INIT] 23/10/11 12:19 WIMF 메인페이지, 기본설정 첫 커밋

---
+++ src/main/java/com/dog/save/WimfApplication.java
@@ -0,0 +1,13 @@
+package com.dog.save;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WimfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dog/save/WimfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
